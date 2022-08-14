5c122bdd9cecddaff88315cc5ac9f6bf06e3423d
minh-nguyen <nguyenkhanhminh1993@gmail.com>
2022-08-14 13:01:05 +0700

    add src

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=udagramdev2.clkn85qad4it.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagramdev2
+export AWS_BUCKET=my-470095975731-bucket-pj3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=udacity
+export JWT_SECRET=hello
+export URL=http://localhost:8100
---
+++ set_env_window.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+set POSTGRES_USERNAME=postgres
+set POSTGRES_PASSWORD=postgres
+set POSTGRES_HOST=udagramdev2.clkn85qad4it.us-east-1.rds.amazonaws.com
+set POSTGRES_DB=udagramdev2
+set AWS_BUCKET=my-470095975731-bucket-pj3
+set AWS_REGION=us-east-1
+set AWS_PROFILE=udacity
+set JWT_SECRET=hello
+set URL=http://localhost:8100
