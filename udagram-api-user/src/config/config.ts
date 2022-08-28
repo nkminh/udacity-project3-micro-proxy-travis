b33be938a3f2ed40b92c0ed0bd5d9859606bd3a7
minh-nguyen <nguyenkhanhminh1993@gmail.com>
2022-08-29 00:55:41 +0700

    delete variable env

--- udagram-api-user/src/config/config.ts
+++ udagram-api-user/src/config/config.ts
@@ -12,17 +12,3 @@ export const config = {
     'secret': process.env.JWT_SECRET,
   },
 };
-// export const config =  {
-//   "username": "postgres",
-//   "password": "postgres",
-//   "database": "udagramdev2",
-//   "host": "udagramdev2.clkn85qad4it.us-east-1.rds.amazonaws.com",
-//   "dialect": "postgres",
-//   "aws_region": "us-east-1",
-//   "aws_profile": "udacity",
-//   "aws_media_bucket": "my-470095975731-bucket-pj3",
-//   'url': "http://localhost:8100",
-//   'jwt': {
-//     'secret': "hello",
-//   },
-// }
